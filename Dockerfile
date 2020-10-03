da180d92e322864c8bd4fae193f1bae36a641bae
Jeff Peeler <jpeeler@gmail.com>
2020-10-03 16:17:31 -0400

    rewrite to use parameters and commands
    Rewritten to split apart credential creation and message sending into
    separate commands. Each command has parameters to make things generic.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:alpine3.12 AS builder
 RUN apk add libffi-dev olm-dev musl-dev gcc
-RUN pip install "matrix-nio[e2e]"
+RUN pip install "matrix-nio[e2e]" click requests
 
 FROM python:alpine3.12
+RUN apk add olm-dev
 COPY --from=builder /usr/local/lib/python3.8/site-packages /usr/local/lib/python3.8/site-packages
-COPY send-message.py /app/send-message.py
+COPY --chmod=755 send-message.py /app/send-message.py
+
+ENTRYPOINT ["/app/send-message.py"]
--- send-message.py
+++ send-message.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3
 
 import asyncio
 import json
 import os
 import sys
-import getpass
+import time
 
-from nio import AsyncClient, LoginResponse
+import click
+import requests
+from nio import AsyncClient, LoginResponse, RoomSendResponse
+from requests.api import request
 
-CONFIG_FILE = "credentials.json"
-
-# Check out main() below to see how it's done.
+CONFIG_FILE = "config.json"
 
 
-def write_details_to_disk(resp: LoginResponse, homeserver) -> None:
+@click.group()
+def cli():
+    pass
+
+
+def write_details_to_disk(
+    resp: LoginResponse, homeserver, full_path, roomid, endpoint
+) -> None:
     """Writes the required login details to disk so we can log in later without
     using a password.
 
@@ -22,80 +30,161 @@ def write_details_to_disk(resp: LoginResponse, homeserver) -> None:
         homeserver -- URL of homeserver, e.g. "https://matrix.example.org"
     """
     # open the config file in write-mode
-    with open(CONFIG_FILE, "w") as f:
+    with open(full_path, "w") as f:
         # write the login details to disk
+        to_write = {
+            "homeserver": homeserver,  # e.g. "https://matrix.example.org"
+            "user_id": resp.user_id,  # e.g. "@user:example.org"
+            "device_id": resp.device_id,  # device ID, 10 uppercase letters
+            "access_token": resp.access_token,  # cryptogr. access token
+        }
+        if roomid:
+            to_write["room_id"] = roomid
+        if endpoint:
+            to_write["endpoint"] = endpoint
         json.dump(
-            {
-                "homeserver": homeserver,  # e.g. "https://matrix.example.org"
-                "user_id": resp.user_id,  # e.g. "@user:example.org"
-                "device_id": resp.device_id,  # device ID, 10 uppercase letters
-                "access_token": resp.access_token,  # cryptogr. access token
-            },
+            to_write,
             f,
         )
 
 
-async def main() -> None:
-    # If there are no previously-saved credentials, we'll use the password
-    if not os.path.exists(CONFIG_FILE):
-        print(
-            "First time use. Did not find credential file. Asking for "
-            "homeserver, user, and password to create credential file."
-        )
-        homeserver = "https://matrix.example.org"
-        homeserver = input(f"Enter your homeserver URL: [{homeserver}] ")
+@cli.command()
+@click.option("--homeserver", required=True, help="Matrix homeserver")
+@click.option("--userid", required=True, help="User ID for message to originate")
+@click.option(
+    "--device-name",
+    default="matrix-nio-messenger",
+    help="Device name associated with user",
+)
+@click.option(
+    "--password", prompt=True, hide_input=True, help="Password associated with user"
+)
+@click.option("--path", default=".", help="Path to credentials")
+@click.option("--roomid", help="Store room ID for later use")
+@click.option("--endpoint", help="Store health endpoint for later use")
+@click.option(
+    "--force/--no-force", default=False, help="Overwrite existing configuration details"
+)
+def init(*args, **kwargs) -> None:
+    asyncio.get_event_loop().run_until_complete(do_init(*args, **kwargs))
 
-        if not (homeserver.startswith("https://") or homeserver.startswith("http://")):
-            homeserver = "https://" + homeserver
 
-        user_id = "@user:example.org"
-        user_id = input(f"Enter your full user ID: [{user_id}] ")
+async def do_init(
+    homeserver, userid, device_name, password, path, roomid, endpoint, force
+) -> None:
+    full_path = os.path.join(path, CONFIG_FILE)
+    if os.path.exists(full_path):
+        if not force:
+            print("Config file already exists, requires --force")
+            return
+        os.remove(full_path)
 
-        device_name = "matrix-nio"
-        device_name = input(f"Choose a name for this device: [{device_name}] ")
+    if not (homeserver.startswith("https://") or homeserver.startswith("http://")):
+        homeserver = "https://" + homeserver
 
-        client = AsyncClient(homeserver, user_id)
-        pw = getpass.getpass()
+    client = AsyncClient(homeserver, userid)
+    resp = await client.login(password, device_name=device_name)
 
-        resp = await client.login(pw, device_name=device_name)
-
-        # check that we logged in succesfully
-        if isinstance(resp, LoginResponse):
-            write_details_to_disk(resp, homeserver)
-        else:
-            print(f'homeserver = "{homeserver}"; user = "{user_id}"')
-            print(f"Failed to log in: {resp}")
-            sys.exit(1)
-
-        print(
-            "Logged in using a password. Credentials were stored.",
-            "Try running the script again to login with credentials.",
-        )
-
-    # Otherwise the config file exists, so we'll use the stored credentials
+    # check that we logged in succesfully
+    if isinstance(resp, LoginResponse):
+        write_details_to_disk(resp, homeserver, full_path, roomid, endpoint)
     else:
-        # open the file in read-only mode
-        with open(CONFIG_FILE, "r") as f:
-            config = json.load(f)
-            client = AsyncClient(config["homeserver"])
+        print(f'homeserver = "{homeserver}"; user = "{userid}"')
+        print(f"Failed to log in: {resp}")
+        sys.exit(1)
 
-            client.access_token = config["access_token"]
-            client.user_id = config["user_id"]
-            client.device_id = config["device_id"]
-
-        # Now we can send messages as the user
-        room_id = "!myfavouriteroomid:example.org"
-        room_id = input(f"Enter room id for test message: [{room_id}] ")
-
-        await client.room_send(
-            room_id,
-            message_type="m.room.message",
-            content={"msgtype": "m.text", "body": "Hello world!"},
-        )
-        print("Logged in using stored credentials. Sent a test message.")
-
-    # Either way we're logged in here, too
+    print(
+        "Logged in using a password. Credentials were stored.",
+    )
     await client.close()
 
 
-asyncio.get_event_loop().run_until_complete(main())
+@cli.command()
+@click.option("--roomid", help="Room ID to deliver message")
+@click.option("--message", required=True, help="Message to deliver")
+@click.option(
+    "--path", default=".", help="Path to credentials"
+)  # should match init option
+@click.option("--endpoint", help="health check endpoint to wait to report success")
+def sendmsg(*args, **kwargs):
+    asyncio.get_event_loop().run_until_complete(do_sendmsg(*args, **kwargs))
+
+
+async def do_sendmsg(roomid, message, path, endpoint) -> None:
+    full_path = os.path.join(path, CONFIG_FILE)
+    if not os.path.exists(full_path):
+        print(f"Didn't find {full_path}, perhaps you should run init first")
+        return
+
+    # open the file in read-only mode
+    with open(full_path, "r") as f:
+        config = json.load(f)
+        client = AsyncClient(config["homeserver"])
+        client.access_token = config["access_token"]
+        client.user_id = config["user_id"]
+        client.device_id = config["device_id"]
+
+        if not roomid:
+            try:
+                roomid = config["room_id"]
+            except KeyError:
+                print("Must either specify roomid or put it config file")
+                raise
+
+        if endpoint:
+            do_check_health(endpoint, 10)
+        else:
+            try:
+                endpoint = config["endpoint"]
+            except KeyError:
+                pass
+            else:
+                do_check_health(endpoint, 10)
+
+        resp = await client.room_send(
+            roomid,
+            message_type="m.room.message",
+            content={
+                "msgtype": "m.text",
+                "body": message,
+            },
+        )
+
+    if isinstance(resp, RoomSendResponse):
+        print("Logged in using stored credentials. Sent message.")
+    else:
+        print(f"Bad response: {resp}")
+    await client.close()
+
+
+@cli.command()
+@click.option(
+    "--endpoint",
+    default="http://localhost:8008/health",
+    help="Health endpoint to query",
+)
+@click.option("--attempts", default=10, help="Number of health check attempts")
+def check_health(*args, **kwargs) -> bool:
+    do_check_health(*args, **kwargs)
+
+
+def do_check_health(endpoint, attempts) -> bool:
+    for x in range(1, attempts + 1):
+        try:
+            r = requests.get(endpoint, timeout=5)
+        except (requests.Timeout, requests.ConnectionError) as e:
+            print(f"Unable to connect to {endpoint}... attempt {x}/{attempts}")
+            pass
+        else:
+            if r.ok:
+                print("Health OK")
+                return True
+            else:
+                r.raise_for_status()
+        time.sleep(2)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    cli()
